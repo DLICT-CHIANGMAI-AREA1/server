7a9717bfb2ca3f94077a9a86c156b4a350cda7ff
artjitt9611 <artjitt2512@gmail.com>
2022-12-06 15:41:29 +0700

    web server

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const express = require("express");
 const app = express();
 const cors = require('cors')
 const router = require("./routes");
-const PORT = process.env.PORT || 5000
+const PORT = process.env.PORT || 7000
 
 
 app.use(cors())
