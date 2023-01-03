baf43c22e3ec9018d144c88d71542582e2cbf1b7
artjitt9611 <artjitt2512@gmail.com>
2023-01-03 15:55:46 +0700

    swagger

--- routes/auth.js
+++ routes/auth.js
@@ -1,8 +1,79 @@
 const router = require("express").Router();
 const { register, login } = require("../controller/auth.controller");
+/**
+ * @swagger
+ * components:
+ *   schemas:
+ *     ยืนยันตัวตน:
+ *       type: object
+ *       required:
+ *         - username
+ *         - password
+ *       properties:
+ *         username:
+ *           type: string
+ *           description: username
+ *         password:
+ *           type: string
+ *           description: password
+ *       example:
+ *         username: David
+ *         password: 1234
+ */
+/**
+ * @swagger
+ * tags:
+ *   name: ยืนยันตัวตน
+ */
+
+/**
+ * @swagger
+ * /admin/api/register:
+ *   post:
+ *     summary: Create a new admin
+ *     tags: [ยืนยันตัวตน]
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             $ref: '#/components/schemas/ยืนยันตัวตน'
+ *     responses:
+ *       200:
+ *         description:   successfully created
+ *         content:
+ *           application/json:
+ *             schema:
+ *               $ref: '#/components/schemas/ยืนยันตัวตน'
+ *       500:
+ *         description: Some server error
+ */
 
 router.post("/api/register", register);
 router.post("/api/login", login);
+
+/**
+ * @swagger
+ * /admin/api/login:
+ *   post:
+ *     summary: Create a new admin
+ *     tags: [ยืนยันตัวตน]
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             $ref: '#/components/schemas/ยืนยันตัวตน'
+ *     responses:
+ *       200:
+ *         description:  successfully created
+ *         content:
+ *           application/json:
+ *             schema:
+ *               $ref: '#/components/schemas/ยืนยันตัวตน'
+ *       500:
+ *         description: Some server error
+ */
 
 /*router.get("/api/me", authenticateJWT, (req, res) => {
     res.send(req.user);
--- server.js
+++ server.js
@@ -3,7 +3,30 @@
 const express = require("express");
 const expressUploader = require("express-fileupload");
 const app = express();
+const swaggerUI = require("swagger-ui-express");
+const swaggerJsDoc = require("swagger-jsdoc");
+//////////////////////////////////////////////////////////////////////////////// swagger config
 
+const options = {
+	definition: {
+		openapi: "3.0.0",
+		info: {
+			title: "DLICT API",
+			version: "1.0.0",
+			description: " Express DLICT API",
+		},
+		servers: [
+			{
+				url: "https://dlictchaingmaiarea1server.onrender.com",
+			},
+		],
+	},
+	apis: ["./routes/*.js"],
+};
+const specs = swaggerJsDoc(options);
+app.use("/api-docs", swaggerUI.serve, swaggerUI.setup(specs));
+
+////////////////////////////////////////////////////////////////////////////////
 require('dotenv').config();
 const cors = require("cors");
 const router = require("./routes");
@@ -20,3 +43,4 @@ app.use(router);
 const listener = app.listen(PORT, () => {
     console.log("Server is running on port " + listener.address().port);
 });
+
