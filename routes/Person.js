d89688bf6ab52267531e8d7fdf0921f79f2cf80a
artjitt9611 <artjitt2512@gmail.com>
2023-01-10 10:08:35 +0700

    update person

--- routes/Person.js
+++ routes/Person.js
@@ -10,7 +10,7 @@ const {
 router.get("/api/DataPerson", DataPerson);
 router.get("/api/DataPersonById/:id", DataPersonById);
 router.delete("/api/DeletePerson/:id", DeletePerson);
-router.put("/api/UpdatePerson/:id", UpdatePerson);
+router.post("/api/UpdatePerson/:id", UpdatePerson);
 router.post("/api/CreatePerson/", CreatePerson);
 
 /**
